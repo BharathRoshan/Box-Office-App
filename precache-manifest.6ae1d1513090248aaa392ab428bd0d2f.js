92303838f1a092049bfed2bd7771da0efb840710
BharathRoshan <bharathroshan1998@gmail.com>
2021-03-01 13:25:33 +0530

    Updates

---
+++ precache-manifest.6ae1d1513090248aaa392ab428bd0d2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d5f8dc91ca51f376e08a6753b9dda5e",
+    "url": "/Box-Office-App/index.html"
+  },
+  {
+    "revision": "05983036eda83e9f3dac",
+    "url": "/Box-Office-App/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "5817cae0183338c0314e",
+    "url": "/Box-Office-App/static/js/2.e479b66c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Box-Office-App/static/js/2.e479b66c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05983036eda83e9f3dac",
+    "url": "/Box-Office-App/static/js/main.7d305b6b.chunk.js"
+  },
+  {
+    "revision": "4c7aadfdeed96a43d7cc",
+    "url": "/Box-Office-App/static/js/runtime-main.57b755fa.js"
+  }
+]);
